414e2796b4ac3f721a4f8025c9733f96da57c4cc
SYSTEX\2200746 <johnlai@systex.com>
2022-11-18 13:18:30 +0800

    初始化Soring Boot Mall

---
+++ src/main/java/com/jlyhsgit/controller/ProductController.java
@@ -0,0 +1,13 @@
+package com.jlyhsgit.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+    @GetMapping("/product")
+    public String getProduct(){
+        return "1";
+    }
+}
